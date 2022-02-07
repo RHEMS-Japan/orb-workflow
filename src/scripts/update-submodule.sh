d9fe304b6d9a083fc9be78ab47d752c4e5138239
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-02-07 11:37:34 +0900

    [semver:skip] Change script to external file

---
+++ src/scripts/update-submodule.sh
@@ -0,0 +1,22 @@
+if [ -n ${MODULE_NAME} ]; then
+  module_name=$(eval echo ${MODULE_NAME})
+  commit_message="$(eval echo ${COMMIT_MESSAGE}): ${module_name}"
+
+  _key=$(echo ${SUBM_FINGER_PRINT} | sed -e 's/://g')
+  export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa_${_key}"
+  git config --global user.email "submodule.updater@rhems-japan.co.jp"
+  git config --global user.name "submodule-updater"
+
+  git submodule update --init --remote --recursive ${module_name}
+  git status
+
+  git checkout ${CIRCLE_BRANCH}
+  _key=$(echo ${MASTER_FINGER_PRINT} | sed -e 's/://g')
+  export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa_${_key}"
+  git branch --set-upstream-to=origin/${CIRCLE_BRANCH} ${CIRCLE_BRANCH}
+  git pull
+  git commit -a -m "${commit_message}" || true
+  git push -u origin ${CIRCLE_BRANCH}
+  else
+  echo "target not found."
+fi
