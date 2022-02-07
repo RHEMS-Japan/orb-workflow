4f12c7c3416603f130988d2c541257708fd34a9a
ichi-01 <ichikawa@rhems-japan.co.jp>
2022-02-07 14:48:08 +0900

    [semver:skip] empty .gitmodule test

--- src/scripts/update-submodule.sh
+++ src/scripts/update-submodule.sh
@@ -17,19 +17,20 @@ if [ -n ${MODULE_NAME} ]; then
       # 指定のモジュール名が存在するか
       branch_name=""
       if [[ $paths =~ $module_name ]]; then
-          echo $module_name
           # 指定のモジュール名のpathが何行目か
           N=$(grep -n "path = $module_name" .gitmodules | sed -e 's/:.*//g')
-          echo $N
           # 指定のモジュールのブランチ名の取得
           branch_name=$(awk "NR==$N+2" .gitmodules | awk '{print $3}')
           echo $branch_name
-      fi
-      # 現在のブランチ名と異なったら
-      if [ $branch_name != ${CIRCLE_BRANCH} ]; then
-        # 一旦deinitして設定し直す
-        git submodule deinit $module_name
-        git rm $module_name
+          # 現在のブランチ名と異なったら
+          if [ $branch_name != ${CIRCLE_BRANCH} ]; then
+              # 一旦deinitして設定し直す
+              git submodule deinit -f $module_name
+              git rm -f $module_name
+              git submodule add --quiet --force -b ${CIRCLE_BRANCH} ${submodule_url}
+          fi
+      else
+        echo -e "no setting in .gitmodule\n"
         git submodule add --quiet --force -b ${CIRCLE_BRANCH} ${submodule_url}
       fi
   # .gitmodulesが存在しなかったら
